5ccf303594ef7ffdcdb3ff5b716d0f714a56a8ff
Basant <basant.kumar@gslab.com>
2018-12-04 08:53:32 +0530

    add custom login, logout and password reset functionality

--- mainStackApp/mainStackApp/urls.py
+++ mainStackApp/mainStackApp/urls.py
@@ -14,12 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('login/', auth_views.login,{'template_name': 'registration/login.html'}, name='login'),
-    path('logout/', auth_views.logout, name='logout'),
+    # path('login/', auth_views.login,{'template_name': 'registration/login.html'}, name='login'),
+    # path('logout/', auth_views.logout, name='logout'),
+    # path('login/home/', admin.site.urls ),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
 
 ]
