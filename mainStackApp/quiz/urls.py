b592a068715da782dccdf604b342695e63f66411
Basant <basant.kumar@gslab.com>
2018-12-06 11:22:55 +0530

    add urls to access all questions, upvote and downvote question using rate_answer, rate_question and view all answer of a specific question

---
+++ mainStackApp/quiz/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from django.views.generic import TemplateView
+from quiz.views import QuestionListView, QuestionCreateView, QuestionDetailView, rate_question, rate_answer
+
+urlpatterns = [
+                path('all/', QuestionListView.as_view(),name='question_list'),
+                path('question/<slug>/',QuestionDetailView.as_view(),
+                           name='question_detail'),
+                path('rate_question/<object_id>/<score>/',
+                           rate_question, name='question_rate'),
+                path('rate_answer/<object_id>/<score>/',
+                           rate_answer, name='answer_rate'),
+
+]
