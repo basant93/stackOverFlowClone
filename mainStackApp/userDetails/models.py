4b3cddd77cd726705f7046fce409f4fc4d5b698e
Basant <basant.kumar@gslab.com>
2018-12-06 11:57:39 +0530

    extend django User model with UserProfile model and a field reputation to maintain reputaion per user. Add post_signal method to update the reputation field after save of Question or Answer model is called

--- mainStackApp/userDetails/models.py
+++ mainStackApp/userDetails/models.py
@@ -4,14 +4,38 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.db.models import signals
+from quiz.models import Question, Answer
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.shortcuts import get_object_or_404
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     reputation = models.IntegerField(default=0)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.user.username
 
-    def get_absolute_url(self):
-        return reverse('user_detail', kwargs={'username': self.user.username})
 
+@receiver(post_save, sender=Answer)
+def add_answer_reputation(sender, instance, **kwargs):
+    """
+    Increase reputation for every answer given.
+    """
+    user_id = instance.author_id
+    user_obj = get_object_or_404(User, id=user_id)
+    user_obj.reputation +=1
+    user_obj.save()
+
+
+@receiver(post_save, sender=Question)
+def add_question_reputation(sender, instance, **kwargs):
+    """
+    Increase reputation for every question asked.
+    """
+    user_id = instance.author_id
+    user_obj = get_object_or_404(User, id=user_id)
+    user_obj.reputation +=1
+    user_obj.save()
+    
